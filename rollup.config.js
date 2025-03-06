1d078cf3216c7115c5d94823c2fca62ca35bc01c
Yuri Sulyma <yuri@liqvidjs.org>
2025-03-05 20:03:43 -0700

    install Biome

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
+import terser from "@rollup/plugin-terser";
 // rollup.config.js
 import typescript from "@rollup/plugin-typescript";
-import terser from "@rollup/plugin-terser";
 
 export default {
   input: "src/index.ts",
